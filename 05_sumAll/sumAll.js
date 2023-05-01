62b90cdc6009ed0351399e40b0d9a7b6e00ab0de
Luke Moule <65358800+LukeMoule@users.noreply.github.com>
2023-05-01 11:08:42 +0100

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(l, s) {
 
+    if ( l<0 || s<0){
+        return "ERROR";
+    }
+    if (!Number.isInteger(l) || !Number.isInteger(s)) return "ERROR";
+
+    if (s>l){
+        let foo = l;
+        l = s;
+        s = foo;
+    }
+    
+    let sum = 0;
+
+
+    for (let i = s ; i<= l; i++){
+        sum += i;
+    }   
+
+    return sum;
 };
 
 // Do not edit below this line
