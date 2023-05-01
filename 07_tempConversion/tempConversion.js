b463d57fc3deede8f8d80157df8d3a1cf1cf38b3
Luke Moule <65358800+LukeMoule@users.noreply.github.com>
2023-05-01 11:19:29 +0100

    completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(farenheit) {
+  let celsius = (farenheit - 32) * (5/9);
+  return round(celsius, 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit = (celsius * (9/5)) + 32;
+  return round(farenheit, 1)
 };
 
 // Do not edit below this line
